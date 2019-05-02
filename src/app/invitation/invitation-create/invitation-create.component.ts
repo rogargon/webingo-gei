e0a5bdad7d4676ead8236d7fb856501ee2382fb9
lrcarrera <drewico@gmail.com>
2019-05-02 16:26:03 +0200

    Add Create Invitation component

---
+++ src/app/invitation/invitation-create/invitation-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invitation-create',
+  templateUrl: './invitation-create.component.html',
+  styleUrls: ['./invitation-create.component.css']
+})
+export class InvitationCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
