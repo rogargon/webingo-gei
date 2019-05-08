0e379e871f332c219276ca5492b09ae689cc111e
Alvaro Ortega <alortegama@outlook.com>
2019-05-08 17:54:10 +0200

    -Added sweetAngular2 for angular, now we can execute swal from html code. -Recover correctly data of admins -Catch error user doesn't have Card

--- src/app/user/user-detail/admin-detail.component.ts
+++ src/app/user/user-detail/admin-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AdminService } from '../admin.service';
-import { Admin } from '../admin';
-import Swal from "sweetalert2";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AdminService} from '../admin.service';
+import {Admin} from '../admin';
+import Swal from 'sweetalert2';
 import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
 
 @Component({
@@ -21,42 +21,25 @@ export class AdminDetailComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.adminService.get(id).subscribe(
-      admin => this.user = admin);
+      admin => {
+        this.user = admin;
+      }
+    )
+    ;
   }
-  isAdmin(){
+
+  isAdmin() {
     return this.authenticationService.isAdmin();
   }
 
   public delete() {
-
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
-      },
-      buttonsStyling: false,
-    });
-
-    swalWithBootstrapButtons.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonClass:"m-1",
-      confirmButtonText: 'Delete it!',
-      cancelButtonClass:"m-1",
-      cancelButtonText: 'Cancel!',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.value) {
-        this.adminService.delete(this.user).subscribe(
-          () => this.router.navigate(['users']));
-        swalWithBootstrapButtons.fire(
-          'Deleted!',
-          'The player ' + this.user.username + ' has been deleted',
-          'success'
-        )
-      }
-    })
+    this.adminService.delete(this.user).subscribe(
+      () => this.router.navigate(['users']));
+    Swal.fire(
+      'Deleted!',
+      'The user ' + this.user.username + ' has been deleted',
+      'success'
+    );
   }
+
 }
--- src/app/user/user-detail/player-detail.component.ts
+++ src/app/user/user-detail/player-detail.component.ts
@@ -11,7 +11,7 @@ import {AuthenticationBasicService} from '../../login-basic/authentication-basic
   templateUrl: './user-detail.component.html'
 })
 export class PlayerDetailComponent implements OnInit {
-  public player: Player = new Player();
+  public user: Player = new Player();
 
   constructor(private route: ActivatedRoute,
               private playerService: PlayerService,
@@ -24,12 +24,10 @@ export class PlayerDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.playerService.get(id).subscribe(
       player => {
-        this.player = player;
-        this.cardService.getBySelfLink(player._links.card.href).subscribe(
-          card => {
-            this.player.card = card;
-          }
-        );
+        this.cardService.getBySelfLink(player._links.card.href).toPromise()
+          .then(card => this.user.card = card)
+          .catch(() => console.log('User doesn\'t have card'));
+        this.user = player;
       });
   }
 
@@ -38,36 +36,12 @@ export class PlayerDetailComponent implements OnInit {
   }
 
   public delete() {
-
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
-      },
-      buttonsStyling: false,
-    });
-
-    swalWithBootstrapButtons.fire({
-      title: 'Are you sure?',
-      text: 'You won\'t be able to revert this!',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonClass: 'm-1',
-      confirmButtonText: 'Delete it!',
-      cancelButtonClass: 'm-1',
-      cancelButtonText: 'Cancel!',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.value) {
-        this.playerService.delete(this.player).subscribe(
-          () => this.router.navigate(['users']));
-        swalWithBootstrapButtons.fire(
-          'Deleted!',
-          'The player ' + this.player.username + ' has been deleted',
-          'success'
-        );
-      }
-    });
-
+    this.playerService.delete(this.user).subscribe(
+      () => this.router.navigate(['users']));
+    Swal.fire(
+      'Deleted!',
+      'The user ' + this.user.username + ' has been deleted',
+      'success'
+    );
   }
 }
