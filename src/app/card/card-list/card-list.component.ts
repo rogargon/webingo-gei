9fd7e306daa75f6d3e61d2afbf50d8e39964b586
Pablo Vilas <pablovilas97@gmail.com>
2019-05-08 16:07:40 +0200

    Card List changed

--- src/app/card/card-list/card-list.component.ts
+++ src/app/card/card-list/card-list.component.ts
@@ -6,7 +6,6 @@ import { CardService } from '../card.service';
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent implements OnInit {
   public cards: Card[] = [];
