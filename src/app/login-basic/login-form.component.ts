840989622b2763a4108af457165fb150d8040329
Alvaro Ortega <alortegama@outlook.com>
2019-05-10 10:03:30 +0200

    Change errorMessage to SweetAlert. Web more beautiful

--- src/app/login-basic/login-form.component.ts
+++ src/app/login-basic/login-form.component.ts
@@ -34,7 +34,7 @@ export class LoginFormComponent implements OnInit {
           Toast.fire({
             type: 'success',
             title: 'Signed in successfully'
-          })
+          });
         });
 
   }
