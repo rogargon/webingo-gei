34b35b5aa7f450220463e32237a5eb89931a802a
Alvaro Ortega <alortegama@outlook.com>
2019-05-10 11:07:52 +0200

    Catch error we don't get respond from server

--- src/app/error-handler/http-error-interceptor.ts
+++ src/app/error-handler/http-error-interceptor.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {ErrorMessageService} from './error-message.service';
-import { Observable } from 'rxjs/internal/Observable';
-import { tap } from 'rxjs/operators';
+import {Observable} from 'rxjs/internal/Observable';
+import {tap} from 'rxjs/operators';
+import Swal from 'sweetalert2';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -14,15 +15,32 @@ export class HttpErrorInterceptor implements HttpInterceptor {
 
     return next.handle(request).pipe(
       tap(
-      () => {},
-      (error: any) => {
-        if (error instanceof HttpErrorResponse) {
-          console.log('HTTP Error Interceptor: ' + this.extractErrorMessage(error));
-          if (error.status !== 404) {
-            this.errorMessageService.showErrorMessage(this.extractErrorMessage(error));
+        () => {
+        },
+        (error: any) => {
+          if (error instanceof HttpErrorResponse) {
+            console.log('HTTP Error Interceptor: ' + this.extractErrorMessage(error));
+            if (error.status === 0) {
+              const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000
+              });
+
+              Toast.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'There was an error connecting to Server!',
+              });
+            } else {
+              if (error.status !== 404) {
+                console.log(error);
+                this.errorMessageService.showErrorMessage(this.extractErrorMessage(error));
+              }
+            }
           }
-        }
-      })
+        })
     );
   }
 
