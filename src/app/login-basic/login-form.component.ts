8e6f1c470ab453d1e2ecfcd539fecb16a99cf72f
Alvaro Ortega <alortegama@outlook.com>
2019-05-12 18:57:48 +0200

    Validate Login Form in real time, before click submit button.

--- src/app/login-basic/login-form.component.ts
+++ src/app/login-basic/login-form.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationBasicService} from './authentication-basic.service';
 import {Location} from '@angular/common';
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
+import {User} from './user';
 
 @Component({
   selector: 'app-login-form',
@@ -9,18 +11,32 @@ import Swal from "sweetalert2";
   styleUrls: [],
 })
 export class LoginFormComponent implements OnInit {
+  user: User;
 
   constructor(private authenticationService: AuthenticationBasicService,
               private location: Location) {
   }
 
   ngOnInit() {
+    this.user = new User();
   }
 
-  onSubmit(userInput: HTMLInputElement, passwordInput: HTMLInputElement): void {
+  onSubmit(loginForm: NgForm): void {
+    if (loginForm.invalid) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000
+      });
 
-
-    this.authenticationService.login(userInput.value, passwordInput.value)
+      Toast.fire({
+        type: 'error',
+        title: 'Don\'t touch my code'
+      });
+      return;
+    }
+    this.authenticationService.login(loginForm.controls.username.value, loginForm.controls.password.value)
       .subscribe(
         user => {
           this.authenticationService.storeCurrentUser(user);
