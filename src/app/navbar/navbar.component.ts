f742c7fd8fbd280e4682f0231bca05b8941ead24
Alvaro Ortega <alortegama@outlook.com>
2019-05-12 19:03:13 +0200

    Don't show dropdown if user not loggedIn

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,14 @@ import {Component, OnInit} from '@angular/core';
 export class NavbarComponent implements OnInit {
   public isCollapsed: boolean;
 
-  constructor() {
+  constructor(private authenticationService: AuthenticationBasicService) {
   }
 
   ngOnInit() {
     this.isCollapsed = true;
+  }
+
+  isLogged() {
+    return this.authenticationService.isLoggedIn();
   }
 }
