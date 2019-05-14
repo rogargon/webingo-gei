46858c035fc50c769c9f3445375f6342e2a70ab6
Pablo Vilas <pablovilas97@gmail.com>
2019-05-14 18:45:47 +0200

    Implemented pagination

--- src/app/card/card-list/card-list.component.ts
+++ src/app/card/card-list/card-list.component.ts
@@ -5,6 +5,7 @@ import { CardService } from '../card.service';
 import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
 import {PlayerService} from '../../user/player.service';
 import {logger} from 'codelyzer/util/logger';
+import { Sort } from 'angular4-hal-aot';
 
 @Component({
   selector: 'app-card-list',
@@ -13,8 +14,9 @@ import {logger} from 'codelyzer/util/logger';
 export class CardListComponent implements OnInit {
   public cards: Card[] = [];
   public totalCards = 0;
-  public asd: string;
-
+  public pageSize = 10;
+  public page = 1;
+  private sorting: Sort[] = [{ path: 'id', order: 'ASC' }];
   constructor(
     private cardService: CardService, private authenticationService: AuthenticationBasicService, private playerService: PlayerService) {
   }
@@ -23,12 +25,19 @@ export class CardListComponent implements OnInit {
       if (!this.authenticationService.isAdmin()) {
         this.playerService.findByUsernameContaining(this.authenticationService.getCurrentUser().username)
           .subscribe(player => this.cards = [player[0].card]);
-        this.totalCards = this.cards.length;
+        this.totalCards = this.cardService.totalElement();
       } else {
-        this.cardService.getAll().subscribe(cards => this.cards = cards);
-        this.totalCards = this.cards.length;
+        this.cardService.getAll({size: this.pageSize, sort: this.sorting})
+          .subscribe((cards) => {
+            this.cards = cards;
+            this.totalCards = this.cardService.totalElement();
+          });
       }
+  }
 
+  changePage() {
+    this.cardService.page(this.page - 1).subscribe(
+      (cards: Card[]) => this.cards = cards);
   }
 
   showSearchResults(cards) {
