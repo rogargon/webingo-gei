8acdc9409ed71987042d993bc24f17afab0aacac
yesid <yqz1280@gmail.com>
2019-05-14 17:04:11 +0200

    Edited file component Yesid/ALbert

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {Game} from "../game";
 import {Router} from "@angular/router";
 import {GameService} from "../game.service";
-import {forkJoin} from "rxjs";
 import {Sort} from "angular4-hal-aot";
 
 @Component({
@@ -13,20 +12,20 @@ import {Sort} from "angular4-hal-aot";
 export class GameListComponent implements OnInit {
   public games: Game[] = [];
   public totalGames = 0;
-  public pagesize = 10;
-  public page = 1;
+
   private sorting: Sort[] = [{path:'Name', order:'ASC'}];
 
   constructor(public router: Router,
               private gameService: GameService) { }
 
   ngOnInit() {
-    this.gameService.getAll({size: this.pagesize, sort: this.sorting}).subscribe(
-      (games:Game[]) => {
-        this.games = games;
-        this.totalGames = this.gameService.totalElement();
-      }
-    )
+    this.gameService.getAll()
+      .subscribe(
+        (games) => {
+          this.games = games;
+          this.totalGames = this.games.length;
+          console.log(this.games);
+        });
   }
 
   showSearchResults(games) {
