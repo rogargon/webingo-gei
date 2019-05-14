d420235c7c2e445881dd05c9d1355849dcbd4d18
albeertito7 <albertperezdatsira@gmail.com>
2019-05-14 16:39:55 +0200

    Create Game List Component Yesid/Albert

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Game} from "../game";
+import {Router} from "@angular/router";
+import {GameService} from "../game.service";
+import {forkJoin} from "rxjs";
+import {Sort} from "angular4-hal-aot";
 
 @Component({
   selector: 'app-game-list',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
+  public games: Game[] = [];
+  public totalGames = 0;
+  public pagesize = 10;
+  public page = 1;
+  private sorting: Sort[] = [{path:'Name', order:'ASC'}];
 
-  constructor() { }
+  constructor(public router: Router,
+              private gameService: GameService) { }
 
   ngOnInit() {
+    this.gameService.getAll({size: this.pagesize, sort: this.sorting}).subscribe(
+      (games:Game[]) => {
+        this.games = games;
+        this.totalGames = this.gameService.totalElement();
+      }
+    )
   }
 
+  showSearchResults(games) {
+    this.games = games;
+  }
 }
