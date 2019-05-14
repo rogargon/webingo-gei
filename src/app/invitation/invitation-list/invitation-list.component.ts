d9ea8e06dba89ffefdfef1d09bd035be6d82467a
Oriol Jorge <ori.jorge@gmail.com>
2019-05-14 18:13:28 +0200

    Invitations pagination implemented

--- src/app/invitation/invitation-list/invitation-list.component.ts
+++ src/app/invitation/invitation-list/invitation-list.component.ts
@@ -3,6 +3,7 @@ import {forkJoin} from 'rxjs';
 import {Invitation} from '../invitation';
 import {InvitationService} from '../invitation.service';
 import {AuthenticationBasicService} from '../../login-basic/authentication-basic.service';
+import { Sort } from 'angular4-hal-aot';
 
 
 @Component({
@@ -13,7 +14,10 @@ import {AuthenticationBasicService} from '../../login-basic/authentication-basic
 export class InvitationListComponent implements OnInit {
 
   public invitations: Invitation[] = [];
+  public pageSize = 12;
+  public page = 1;
   public totalInvitations = 0;
+  private sorting: Sort[] = [{ path: 'id', order: 'ASC' }];
 
   constructor(
     private invitationService: InvitationService,
@@ -21,11 +25,11 @@ export class InvitationListComponent implements OnInit {
     }
 
   ngOnInit() {
-      this.invitationService.getAll()
+      this.invitationService.getAll({size: this.pageSize, sort: this.sorting})
       .subscribe(
         (invitations) => {
           this.invitations = invitations;
-          this.totalInvitations = this.invitations.length;
+          this.totalInvitations = this.invitationService.totalElement();
           console.log(this.invitations);
         });
   }
@@ -35,5 +39,10 @@ export class InvitationListComponent implements OnInit {
   }
   isAdmin(): boolean {
     return this.authenticationService && this.authenticationService.isAdmin();
+  }
+
+  changePage() {
+    this.invitationService.page(this.page - 1).subscribe(
+      (invitations: Invitation[]) => this.invitations = invitations);
   }
 }
